98da0017267848704efaba973205d23653b5388f
Jochem Kossen <jochem@jkossen.nl>
2012-02-25 12:31:19 +0100

    check if .exif file exists; use Show in paginated_overview

--- controllers/page_controller.py
+++ controllers/page_controller.py
@@ -11,14 +11,16 @@ class PageController(Controller):
     def image_info(self, album, filename, template='image.html'):
         show = Show(self.app, album)
         exifdir = os.path.join(self.app.config['CACHE_DIR'], album, 'exif')
-        f = open(os.path.join(exifdir, filename + '.exif'))
-        exif_array = f.readlines()
-        f.close()
+        exif_array = []
+        if os.path.exists(os.path.join(exifdir, filename + '.exif')):
+            f = open(os.path.join(exifdir, filename + '.exif'))
+            exif_array = f.readlines()
+            f.close()
 
         return self.render_themed(template, album=album, filename=filename,
                                   exif=exif_array, show=show)
 
-    def paginated_overview(self, album, page, files, endpoint, template):
+    def paginated_overview(self, album, show, page, files, endpoint, template):
         ext = re.compile(".jpg$", re.IGNORECASE)
         all_files = os.listdir(os.path.join(self.app.config['ALBUMS_DIR'], album))
         all_files = filter(ext.search, all_files)
@@ -26,7 +28,7 @@ class PageController(Controller):
 
         p = Paginator(album, files, self.app.config['THUMBNAILS_PER_PAGE'], page, endpoint, template)
         return self.render_themed(template + '.html', album=album,
-                                  files=p.entries, paginator=p, page=page,
+                                  show=show, files=p.entries, paginator=p, page=page,
                                   all_files=all_files)
 
     def get_show(self, album, page, endpoint, template):
@@ -36,7 +38,7 @@ class PageController(Controller):
             abort(404)
 
         files = show.data['files']
-        return self.paginated_overview(album, page, files, endpoint, template)
+        return self.paginated_overview(album, show, page, files, endpoint, template)
 
     def get_nonpaginated_show(self, album, template):
         show = Show(self.app, album)
