cf4792883d41f36c909635aa204d90356469a066
Jochem Kossen <jochem@jkossen.nl>
2012-02-26 18:23:02 +0100

    Refactor admin into blueprint

--- admin/views.py
+++ admin/views.py
@@ -29,35 +29,27 @@ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-This is the admin application code for showoff.
+This is the admin adminlication code for showoff.
 
 """
 
-from flask import Flask, render_template, send_from_directory, abort, url_for, redirect, json, jsonify, request
+from flask import Blueprint, current_app, Flask, render_template, send_from_directory, abort, url_for, redirect, json, jsonify, request
 from controllers import PageController, ImageController
-from werkzeug import cached_property
 from libshowoff import Show, Paginator, supported_exiftags, clear_cache, update_cache, update_exif, is_edited, get_edit_or_original, get_exif, rotate_image
 from ExifTags import TAGS
 
 import os, re, Image
 
-app = Flask(__name__, static_path=None, template_folder='templates/admin')
-app.config.from_pyfile('config.py')
-app.config.from_envvar('SHOWOFF_ADMIN_CONFIG', silent=True)
-
-def get_route(function):
-    """Return complete route based on configuration and routes"""
-    return '/%s%s' % (app.config['ADMIN_PREFIX'],
-                      app.config['ADMIN_ROUTES'][function])
+admin = Blueprint('admin', __name__, template_folder='templates')
 
 def themed(template):
     """Return path to template in configured theme"""
-    return os.path.join(app.config['THEME'], template)
+    return os.path.join(current_app.config.get('THEME'), template)
 
-def _paginated_overview(album, page, endpoint='list', template='grid'):
-    controller = PageController(app)
-    show = Show(app, album)
-    files = os.listdir(os.path.join(app.config['ALBUMS_DIR'], album))
+def _paginated_overview(album, page, endpoint='admin.list', template='grid'):
+    controller = PageController(current_app)
+    show = Show(current_app, album)
+    files = os.listdir(os.path.join(current_app.config['ALBUMS_DIR'], album))
 
     ext = re.compile(".jpg$", re.IGNORECASE)
     files = filter(ext.search, files)
@@ -71,12 +63,12 @@ def _paginated_overview(album, page, endpoint='list', template='grid'):
                           files=files, endpoint=endpoint, template=template)
 
 def _rotate_image(album, filename, steps=1):
-    rotate_image(app, album, filename, steps)
-    for size in app.config['ALLOWED_SIZES']:
+    rotate_image(admin, album, filename, steps)
+    for size in current_app.config['ALLOWED_SIZES']:
         if size != 'full':
-            clear_cache(app, album, filename, size)
+            clear_cache(admin, album, filename, size)
 
-@app.template_filter('exif_table')
+#@current_app.template_filter('exif_table')
 def get_exif_table(exif):
     table = '<table>'
     for line in exif:
@@ -84,58 +76,57 @@ def get_exif_table(exif):
     table = table + '</table>'
     return table
 
-@app.route(get_route('static_files'))
+@admin.route('/static/<path:filename>')
 def static_files(filename):
     """Send static files such as style sheets, JavaScript, etc."""
-    static_path = os.path.join(app.root_path, 'templates', 'admin',
-                               app.config['THEME'], 'static')
+    static_path = os.path.join(admin.root_path, 'templates', current_app.config['THEME'], 'static')
     return send_from_directory(static_path, filename)
 
-@app.route(get_route('show_image'))
+@admin.route('/<album>/image/<filename>/<int:size>/')
 def show_image(album, filename, size=None):
-    controller = ImageController(app)
+    controller = ImageController(current_app)
     return controller.act('get', album, filename, size)
 
-@app.route(get_route('show_image_full'))
+@admin.route('/<album>/image/<filename>/full/')
 def show_image_full(album, filename):
     return show_image(album, filename)
 
-@app.route(get_route('image_page'))
+@admin.route('/<album>/show/<filename>')
 def image_page(album, filename):
-    controller = PageController(app)
+    controller = PageController(current_app)
     return controller.act('image_info', album, filename)
 
-@app.route(get_route('rotate_url'))
+@admin.route('/<album>/rotate_exif/')
 def rotate_url():
     # dummy
     pass
 
-@app.route(get_route('list_templated'))
-@app.route(get_route('list'))
+@admin.route('/<album>/list/<template>/<int:page>/')
+@admin.route('<album>/list/<int:page>/')
 def list(album, page, template='grid'):
-    return _paginated_overview(album, page, 'list', template)
+    return _paginated_overview(album, page, 'admin.list', template)
 
-@app.route(get_route('show_album'))
+@admin.route('/<album>/')
 def show_album(album):
     return list(album, 1)
 
-@app.route(get_route('index'))
+@admin.route('/')
 def show_index():
-    dir_list = os.listdir(app.config['ALBUMS_DIR'])
+    dir_list = os.listdir(current_app.config['ALBUMS_DIR'])
     album_list = [os.path.basename(album) for album in dir_list]
     album_list.sort(reverse=True)
     return render_template(themed('index.html'), albums=album_list)
 
-@app.route(get_route('image_rotate'))
+@admin.route('/<album>/rotate/<int:steps>/<filename>/')
 def image_rotate(album, filename, steps=1):
     _rotate_image(album, filename, steps)
     return jsonify(result='OK')
 
-@app.route(get_route('exif_rotate_image'))
+@admin.route('/<album>/rotate_exif/<filename>/')
 def exif_rotate_image(album, filename):
     orientation_steps = { 3: 2, 6: 1, 8: 3 }
-    if not is_edited(app, album, filename):
-        img = Image.open(os.path.join(app.config['ALBUMS_DIR'], album, filename))
+    if not is_edited(admin, album, filename):
+        img = Image.open(os.path.join(current_app.config['ALBUMS_DIR'], album, filename))
         exif = img._getexif()
         ret = {}
         for tag, value in exif.items():
@@ -147,70 +138,70 @@ def exif_rotate_image(album, filename):
                 _rotate_image(album, filename, orientation_steps[orientation])
     return jsonify(result='OK')
 
-@app.route(get_route('add_image_to_show'))
+@admin.route('/<album>/add_image_to_show/<filename>/')
 def add_image_to_show(album, filename):
     """Add an image to the show"""
-    show = Show(app, album)
+    show = Show(current_app, album)
     if show.add_image(filename):
         return jsonify(result='OK')
     return jsonify(result='Failed')
 
-@app.route(get_route('remove_image_from_show'))
+@admin.route('/<album>/remove_image_from_show/<filename>/')
 def remove_image_from_show(album, filename):
     """Remove an image from the show"""
-    show = Show(app, album)
+    show = Show(current_app, album)
     if show.remove_image(filename):
         return jsonify(result='OK')
     return jsonify(result='Failed')
 
-@app.route(get_route('sort_show_by_exifdate'))
+@admin.route('/<album>/sort_by_exifdate/')
 def sort_show_by_exifdate(album):
     """Sort the show by exif datetime """
-    show = Show(app, album)
+    show = Show(current_app, album)
     show.sort_by_exif_datetime()
     return redirect(request.referrer or url_for('index'))
 
-@app.route(get_route('show_edit_users'))
+@admin.route('/<album>/edit_users/')
 def show_edit_users(album):
-    show = Show(app, album)
+    show = Show(current_app, album)
     users = show.data['users']
     return render_template(themed('edit_users.html'), album=album, show=show, users=users)
 
-@app.route(get_route('add_all_images_to_show'))
+@admin.route('/<album>/add_all/')
 def add_all_images_to_show(album):
-    show = Show(app, album)
+    show = Show(current_app, album)
     show.add_all_images()
     return redirect(request.referrer or url_for('index'))
 
-@app.route(get_route('show_change_setting'))
+@admin.route('/<album>/set/<setting>/<value>/')
 def show_change_setting(album, setting, value):
-    show = Show(app, album)
+    show = Show(current_app, album)
     if (show.change_setting(setting, value) and show.save()):
         return jsonify(result='OK')
     else:
         return jsonify(result='Failed')
 
-@app.route(get_route('show_change_password'), methods=['POST'])
+@admin.route('/<album>/change_password/', methods=['POST'])
 def show_change_password(album):
     if request.method == 'POST':
-        show = Show(app, album)
+        show = Show(current_app, album)
         username = request.form['username']
         password = request.form['password']
-        show.set_user(username, app.config['SECRET_KEY'], password)
+        show.set_user(username, current_app.config['SECRET_KEY'], password)
         if (show.save()):
             return redirect(request.referrer or url_for('index'))
         else:
             return jsonify(result='Failed')
 
-@app.route(get_route('show_remove_user'))
+@admin.route('/<album>/remove_user/<username>/')
 def show_remove_user(album, username):
-    show = Show(app, album)
+    show = Show(current_app, album)
     show.remove_user(username)
     if (show.save()):
         return redirect(request.referrer or url_for('index'))
     else:
         return jsonify(result='Failed')
 
-if __name__ == '__main__':
-    app.run(host=app.config['ADMIN_HOST'], port=app.config['ADMIN_PORT'])
+#if __name__ == '__main__':
+#    admin.run(host=current_app.config['ADMIN_HOST'], port=current_app.config['ADMIN_PORT'])
 
