2e7292da3fe6667279b711542a9cccae7d70e673
Omwah <omwah@oyeh.org>
2013-02-20 20:21:38 -0800

    Make run_*.py scripts respect *_PREFIX variables on config.py

--- run_frontend.py
+++ run_frontend.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.config.from_envvar('SHOWOFF_FRONTEND_CONFIG', silent=True)
 
-app.register_blueprint(frontend, url_prefix='/showoff')
+app.register_blueprint(frontend, url_prefix=app.config['FRONTEND_PREFIX'])
 
 if __name__ == '__main__':
     app.run(host=app.config['FRONTEND_HOST'], port=app.config['FRONTEND_PORT'])
