9ddbf44e8a843bb3dd9bce20346dd92f21eb1838
Jochem Kossen <jochem@jkossen.nl>
2013-08-21 19:03:23 +0200

    flaskext.wtf should be flask.ext.wtf, noticed by hornuda, thanks!

---
+++ showoff/frontend/forms.py
@@ -0,0 +1,7 @@
+# Imports {{{
+from flask.ext.wtf import Form, TextField, PasswordField, Required
+# }}}
+
+class LoginForm(Form):
+    username = TextField('Username', validators=[Required(message='Username is required.')])
+    password = PasswordField('Password', validators=[Required(message='Password is required.')])
