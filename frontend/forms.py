04b6501f64ac1918b96825598dd5ebc123155831
Jochem Kossen <jochem@jkossen.nl>
2013-08-21 19:09:43 +0200

    Move things to showoff/ subdir, so setuptools can do its thing

--- frontend/forms.py
+++
@@ -1,7 +0,0 @@
-# Imports {{{
-from flaskext.wtf import Form, TextField, PasswordField, Required
-# }}}
-
-class LoginForm(Form):
-    username = TextField('Username', validators=[Required(message='Username is required.')])
-    password = PasswordField('Password', validators=[Required(message='Password is required.')])
