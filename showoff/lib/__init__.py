357ff412b9216ba4a2d47ae3eb30e8afad5b979a
Jochem Kossen <jochem@jkossen.nl>
2014-11-24 21:25:07 +0100

    s/cache.py/cache_manager.py/g

--- showoff/lib/__init__.py
+++ showoff/lib/__init__.py
@@ -2,5 +2,5 @@ from showoff.lib.paginator import Paginator
 from showoff.lib.show import Show
 from showoff.lib.authentication import hash_password, validate_password
 from showoff.lib.exif import ExifManager
-from showoff.lib.cache import CacheManager
+from showoff.lib.cache_manager import CacheManager
 from showoff.lib.image import Image
