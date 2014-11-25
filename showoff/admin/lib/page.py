aa6b296f6acbfcff863a9a7e6a538a7dacd229cc
Jochem Kossen <jochem@jkossen.nl>
2014-11-25 20:26:46 +0100

    replace filter call with list comprehension; reverse files if show is set to reverse

--- showoff/admin/lib/page.py
+++ showoff/admin/lib/page.py
@@ -1,17 +1,20 @@
 from flask import abort, current_app
-from showoff.lib import Paginator
+from showoff.lib import Paginator, Show
 import os, re
 
 def _paginated_overview(album, page, endpoint='admin.list', template='grid'):
+    show = Show(album)
     files = os.listdir(os.path.join(current_app.config['ALBUMS_DIR'], album))
 
     ext = re.compile(".(jpg|png|gif|bmp)$", re.IGNORECASE)
-    files = filter(ext.search, files)
+    files = [f for f in files if ext.search(f)]
 
     if len(files) == 0:
         abort(404)
 
     files.sort()
 
-    return Paginator(album, files, current_app.config['THUMBNAILS_PER_PAGE'], page, endpoint, template)
+    if show.get_setting('reverse') == 'yes':
+        files.reverse()
 
+    return Paginator(album, files, current_app.config['THUMBNAILS_PER_PAGE'], page, endpoint, template)
