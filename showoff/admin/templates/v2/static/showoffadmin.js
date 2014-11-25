34f554a60bcdca6fb2b38af71db8aa6d410e8952
Jochem Kossen <jochem@jkossen.nl>
2014-11-25 20:28:34 +0100

    Clean up/beautify html and javascript; make it work

--- showoff/admin/templates/v2/static/showoffadmin.js
+++ showoff/admin/templates/v2/static/showoffadmin.js
@@ -3,13 +3,49 @@ var SHOWOFFADMIN = SHOWOFFADMIN || {};
 (function(ns) {
     'use strict';
 
+    var _images = [];
+
     var _cfg = {
         'base_url': 'http://showoff_admin',
         'album': 'default'
     };
 
-    var init = function(cfg) {
+    var init = function(cfg, images) {
         $.extend(_cfg, cfg);
+        _images = images;
+
+        $('.rotate').on('click', function(event) {
+            event.preventDefault();
+            var d = new Date();
+            var index = $(this).data('index')
+            rotate($(this).data('steps'), $('#img-' + index).data('filename'));
+            $('#img-' + index).src = $('#img-' + index).src + '?' + d.getTime();
+        });
+
+        $('#rotate-all-images').on('click', function(event) {
+            event.preventDefault();
+            return rotate_all_images(0);
+        });
+
+        $('.toggle-publish').each(function(index, value) {
+            var icon = ($(this).data('value') == 'yes') ? 'ok' : 'remove';
+            $(this).html(glyph(icon));
+        });
+
+        $('.toggle-publish').on('click', function(event) {
+            event.preventDefault();
+            return toggle_publish($(this));
+        });
+
+        $('.toggle-bool').each(function(index, value) {
+            var icon = ($(this).data('value') == 'yes') ? 'ok' : 'remove';
+            $(this).html(glyph(icon));
+        });
+
+        $('.toggle-bool').on('click', function(event) {
+            event.preventDefault();
+            return toggle_bool($(this));
+        });
     };
 
     var get = function(key) {
@@ -19,41 +55,80 @@ var SHOWOFFADMIN = SHOWOFFADMIN || {};
     var set = function(key, value) {
         _cfg[key] = value;
     };
-    
+
+    var album_url = function() {
+        return ns.get('base-url') + ns.get('album') + '/';
+    };
+
+    var file_url = function(filename) {
+        return album_url + filename + '/'
+    };
+
     var glyph = function(icon) {
         return '<span class="glyphicon glyphicon-' + icon + '"></span>';
     };
 
-    var toggle_show = function(element, url, new_url, add) {
+    var toggle = function(element, url) {
         $(element).html('please wait ...');
+        var new_value = (element.data('value') == 'yes') ? 'no' : 'yes';
         var result = $.getJSON(url, false, function() {
-            var icon = (add) ? 'ok' : 'remove';
+            var icon = (new_value == 'yes') ? 'ok' : 'remove';
             var new_text = glyph(icon)
-            var new_add = (add) ? 'false' : 'true';
-            var done_html = '<a href="#" onclick=\'return SHOWOFFADMIN.toggle_show("' + element + '", "' + new_url + '", "' + url + '", ' + new_add + ');\'>' + new_text + '</a>';
-            $(element).html(done_html);
+
+            $(element).data('value', new_value);
+            $(element).html(new_text);
         });
-        return false;
     };
 
-    var show_toggle_bool = function(element, setting, value) {
-        $(element).html('please wait ...');
-        var url = ns.get('base_url') + ns.get('album') + '/set/' + setting;
-        var result = $.getJSON(url + '/' + value, false, function() {
-            var icon = (value == 'yes') ? 'ok' : 'remove';
-            var new_text = glyph(icon)
-            var new_value = (value == 'yes') ? 'no' : 'yes';
-            var done_html = '<a href="#" onclick=\'return SHOWOFFADMIN.toggle_bool("' + element + '", "' + setting + '", "' + new_value + '");\'>' + new_text + '</a>';
-            $(element).html(done_html);
-        });
-        return false;
+    var toggle_publish = function(element) {
+        var url = file_url(element.data('filename')) + 'toggle_publish';
+        toggle(element, url);
     };
+
+    var toggle_bool = function(element) {
+        var new_value = (element.data('value') == 'yes') ? 'no' : 'yes';
+        var url = album_url() + 'set/' + element.data('setting')  + '/' + new_value;
+        toggle(element, url)
+    };
+
+    var _rotate_all_images = function(index) {
+        var percentage_complete = Math.round((index * 100) / _images.length);
+        $('#progress').html(percentage_complete + ' % done (' + index + ' of ' + _images.length + ': ' + _images[index] + ')');
+        $.ajax({
+            url: album_url() + 'rotate_exif/' + _images[index] + '/',
+            context: document.body,
+            success: function() {
+                if (index < _images.length-1) {
+                    _rotate_all_images(index+1);
+                } else {
+                    window.location.reload();
+                }
+            }
+        });
+    };
+
+    var rotate_all_images = function(index) {
+        if (confirm('This will rotate all images in the album according to EXIF specs. Are you sure?')) {
+            $.blockUI({ message: '<h1>Rotating all images ...</h1><h2><span id="progress">0 percent complete</span></h2>'});
+            _rotate_all_images(index);
+        }
+    };
+
+    var rotate = function(steps, filename) {
+        var url = file_url(filename) + 'rotate/' + steps + '/';
+        $.blockUI({ message: '<h1>Rotating image ...</h1>'});
+        $.ajax({
+            url: url,
+            context: document.body,
+            success: function() {
+                window.location.reload();
+            }
+        });
+    };
+
 
     // export public functions
     ns.init = init;
     ns.get = get;
     ns.set = set;
-    ns.toggle_show = toggle_show;
-    ns.toggle_bool = show_toggle_bool;
-
 }(SHOWOFFADMIN));
