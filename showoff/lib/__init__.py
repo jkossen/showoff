bf44250cb0ea7d12a86c82526ed4d7b3035030df
Jochem Kossen <jochem@jkossen.nl>
2014-11-25 20:57:29 +0100

    Put Show last since it imports the ones before

--- showoff/lib/__init__.py
+++ showoff/lib/__init__.py
@@ -1,6 +1,6 @@
 from showoff.lib.paginator import Paginator
-from showoff.lib.show import Show
 from showoff.lib.authentication import hash_password, validate_password
 from showoff.lib.exif_manager import ExifManager
 from showoff.lib.cache_manager import CacheManager
 from showoff.lib.image import Image
+from showoff.lib.show import Show
